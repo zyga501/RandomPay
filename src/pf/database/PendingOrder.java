49645548a02da140c61ffea0c3f5596f881df459
unknown <VonChenPlus@gmail.com>
2016-10-16 21:11:14 +0800

    add order info

---
+++ src/pf/database/PendingOrder.java
@@ -0,0 +1,22 @@
+package pf.database;
+
+import java.util.List;
+
+public class PendingOrder extends OrderInfo {
+    public static void main(String[] args) throws Exception {
+        PendingOrder pendingOrder = new PendingOrder();
+        pendingOrder.setOpenid("123");
+        pendingOrder.setAmount(123);
+        boolean ret = insertOrderInfo(pendingOrder);
+    }
+
+    public static List<PendingOrder> getPendingOrder() {
+        String statement = "pf.database.mapping.pendingOrder.getOrderInfo";
+        return Database.Instance().selectList(statement);
+    }
+
+    public static boolean insertOrderInfo(PendingOrder pendingOrder) {
+        String statement = "pf.database.mapping.pendingOrder.insertOrderInfo";
+        return Database.Instance().insert(statement, pendingOrder) == 1;
+    }
+}
