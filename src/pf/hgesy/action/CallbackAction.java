ac27882612f9d280930f84ec5ad055cd011c5da1
VonChenPlus <vonchenplus@gmail.com>
2016-11-03 22:51:04 +0800

    update hgesy

--- src/pf/hgesy/action/CallbackAction.java
+++ src/pf/hgesy/action/CallbackAction.java
@@ -3,5 +3,7 @@ package pf.hgesy.action;
 import framework.action.AjaxActionSupport;
 
 public class CallbackAction extends AjaxActionSupport {
+    public void directPay() throws Exception {
 
+    }
 }
--- src/pf/hgesy/action/PayAction.java
+++ src/pf/hgesy/action/PayAction.java
@@ -2,16 +2,16 @@ package pf.hgesy.action;
 
 import framework.action.AjaxActionSupport;
 import pf.ProjectSettings;
-import pf.hgesy.api.DirectPay;
 import pf.hgesy.api.RequestBean.DirectPayRequestData;
 
 public class PayAction extends AjaxActionSupport {
-    public String directPay() throws Exception {
+    public void directPay() throws Exception {
         DirectPayRequestData directPayRequestData = new DirectPayRequestData();
         directPayRequestData.account = ProjectSettings.getMapData("hgesy").get("account").toString();;
         directPayRequestData.total_fee = "0.10";
-        DirectPay directPay = new DirectPay(directPayRequestData);
-        directPay.postRequest(ProjectSettings.getMapData("hgesy").get("appKey").toString());
-        return AjaxActionComplete();
+        directPayRequestData.buildSign(ProjectSettings.getMapData("hgesy").get("appKey").toString());
+        String redirectUrl = "http://www.hgesy.com:8080/PayMcc/gateway/direct_pay?";
+        redirectUrl += directPayRequestData.buildRequestData();
+        getResponse().sendRedirect(redirectUrl);
     }
 }
